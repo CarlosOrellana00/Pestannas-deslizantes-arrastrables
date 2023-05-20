95ec59411504f61ecc5723908edaca75167e171f
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2023-05-19 20:11:40 -0400

    6

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
 const tabsBox = document.querySelector(".tabs-box");
 
-let isDragging = fasle;
+let isDragging = false;
 
 const dragging = (e) => {
   if(!isDragging) return;
   tabsBox.scrollLeft -= e.movementX;
 }
 
+const dragStop = () => {
+  isDragging = false;
+}
+
 tabsBox.addEventListener("mousedown", () => isDragging = true);
 tabsBox.addEventListener("mousemove",dragging);
+document.addEventListener("mouseup", dragStop);
